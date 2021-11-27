1278c89802802e80bff3802e8d88f62580fdfebf
shen <1593995275@qq.com>
2021-11-27 17:00:53 +0800

    更新部分html页面

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+import json
+
+from flask import Flask, request, render_template, Response
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/add', methods=["POST"])
+def hello_world():
+    data = {
+        "object": None,
+        "msg": "成功",
+        "code": 200,
+        "result": False
+    }
+    return Response(json.dumps(data), content_type='application/json')
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
