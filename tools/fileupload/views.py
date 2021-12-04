e4e7565714f91d8eda3d5166d896df9d20286d3b
shen <1593995275@qq.com>
2021-12-04 11:00:32 +0800

    文件上传功能部分

--- tools/fileupload/views.py
+++ tools/fileupload/views.py
@@ -1,13 +1,15 @@
 import json
 import uuid
 from datetime import datetime
-
+import os
 from flask import Blueprint, request, Response
 
 from ext import db
 from tools.message.models import User
 
 file = Blueprint("file", __name__)
+
+upload_path = os.path.abspath('.' + "/static/files/upload")
 
 
 @file.route("/upload", methods=["POST"])
@@ -25,12 +27,13 @@ def upload():
         return Response(json.dumps(data), content_type="application/json")
     file_id = "".join(str(uuid.uuid1()).split("-"))
     file_name = file_id + "." + file_obj.filename.split(".")[1]
+    print(upload_path)
     user = User()
     user_name = User.query.filter(User.user == user_id).first()
     if user_name is None:
         user.user = user_id
         user.create_time = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-        file_obj.save(file_name)
+        file_obj.save(upload_path + "/" + file_name)
         user.file_name = file_name
         db.session.add(user)
         db.session.commit()
@@ -44,7 +47,7 @@ def upload():
         user_name.update_time = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
         user_name.file_name = file_name
         db.session.commit()
-        file_obj.save(file_name)
+        file_obj.save(upload_path + "/" + file_name)
         data["msg"] = "上传成功"
         data["code"] = 200
         return Response(json.dumps(data), content_type="application/json")
