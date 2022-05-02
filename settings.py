a62bd7bcd41f0e4a28346c32e100ad5efc2b2b83
shen <1593995275@qq.com>
2022-05-02 18:46:45 +0800

    逻辑优化

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ class Config:
 class Development(Config):
     ENV = 'development'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:shenyongfu@127.0.0.1:3306/msg?charset=utf8mb4"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:shen123456@127.0.0.1:3306/msg?charset=utf8mb4"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_RECORD_QUERIES = True
@@ -15,7 +15,7 @@ class Development(Config):
 class Production(Config):
     ENV = 'production'
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:liebao123@10.43.1.7:3306/msg?charset=utf8mb4"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@10.0.0.0:3306/msg?charset=utf8mb4"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_RECORD_QUERIES = False
